e2cc204f2ebf5767ebae44d074ef13785962bf9d
Wolfysteyr <looptyfloops@gmail.com>
2024-09-15 22:34:41 +0300

    php basics

---
+++ to-do-list.php
@@ -0,0 +1,42 @@
+<?php
+$taskList = ["First task", "second task"];
+
+function showTask($inputTasks){
+    foreach ($inputTasks as $task) {
+        echo $task . "\n";
+    }
+}
+
+function addTask(){
+    echo "To be implemented!\n"
+}
+
+do {
+    echo "Uzdevumu parvaldnieks\n";
+    echo "Apskatīt visus uzdevumus => 1\n";
+    echo "Ievadīt jaunu uzdevumu => 2\n";
+    $choise = readline("Izvelaties darbību: ");
+
+    switch ($choise) {
+        case '1':
+            showTask($taskList);
+            break;
+        
+        case '2':
+            echo "To be implemented!\n";
+            break;
+
+        default:
+            echo "Invalid option";
+            break;
+    }
+
+    $loop = readline("Turpināt:\n");
+    if ($loop !="N") {
+       $array[] = "$loop"; 
+    }
+} while ($loop != "N");
+
+var_dump($array);
+
+
